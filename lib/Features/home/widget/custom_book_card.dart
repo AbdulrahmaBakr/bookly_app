093d5e0d31c68a21bdf7554a5ae5c55783c11934
Abdalrahman Bakr <61604696+AbdulrahmaBakr@users.noreply.github.com>
2023-11-27 22:05:34 +0200

    book card

--- lib/Features/home/widget/custom_book_card.dart
+++ lib/Features/home/widget/custom_book_card.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/core/utils/AppAssets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class CustomBookCard extends StatelessWidget {
   const CustomBookCard({
@@ -7,10 +9,34 @@ class CustomBookCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        
-      ],
+    return Container(
+      width: 129,
+      height: 193,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AppAssets.bgTest),
+        ),
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: 15,
+            right: 10,
+            child: Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(73, 255, 255, 255),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.play_arrow),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/core/utils/AppAssets.dart
+++ lib/core/utils/AppAssets.dart
@@ -2,4 +2,6 @@ const String baseSvg = 'assets/images/';
 
 class AppAssets {
   static const appLogo = '${baseSvg}bookly_logo.svg';
+  static const bgTest = '${baseSvg}bg_test.png';
+  static const play = '${baseSvg}play_image.svg';
 }
